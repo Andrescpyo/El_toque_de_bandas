6cda0dcbd79ad99c9ae50bf8e1d3463e38d909c3
FyAguilar <fyaguilarj@udistrital.edu.co>
2023-09-21 23:54:21 -0500

    Actualización

--- banda.py
+++ banda.py
@@ -1,24 +1,24 @@
 from instrumentos import *
 
-class Principal():
+class Main():
     root = tk.Tk()
-    root.title("Banda Aleatoria")
+    root.title("Instrumentos Musicales")
 
-    canvas = tk.Canvas(root, width=900, height=500, bg="white")
+    canvas = tk.Canvas(root, width=900, height=400)  # Ajusta el ancho y alto según tus necesidades
     canvas.pack()
 
-    instrumentos = [Guitarra(), Bajo(), Bateria(), Piano(), Saxofon()]
-    imagenes = []
+    instrumentos = [Guitarra, Bajo, Bateria, Piano, Saxofon]
+    x_spacing = 20
+    images = []
 
     for _ in range(5):
-        inst = choice(instrumentos)
-        imagenes.append(inst.imagen())
-
-    x = 50
-    separacion = 20
-
-    for imagen in imagenes:
-        canvas.create_image(x, 100, image=imagen, anchor=tk.NW)
-        x += imagen.width() + separacion
+        inst_class = choice(instrumentos)
+        inst = inst_class()
+        img = inst.imagen()
+        images.append(img)
+        canvas.create_text(x_spacing + 75, 10, text=f"Tocando {inst.__class__.__name__}", anchor=tk.N)
+        canvas.create_image(x_spacing, 40, image=img, anchor=tk.NW)
+        x_spacing += 150 + 20  # Ajusta el espaciado según tus necesidades
 
     root.mainloop()
+
