5c09823aa440542049cc4132bc5a8834dfa3701a
FyAguilar <142015029+FyAguilar@users.noreply.github.com>
2023-10-20 11:55:12 +0000

    Actualizacion clase Instrumento

--- instrumentos.py
+++ instrumentos.py
@@ -4,36 +4,32 @@ from PIL import Image, ImageTk
 
 class Instrumento():
     def imagen(self):
-        pass
+        self.img = self.img.resize((150, 150))
+        return ImageTk.PhotoImage(self.img)
 
 class Guitarra(Instrumento):
     def imagen(self):
-        img = Image.open("image/Guitarra.png")
-        img = img.resize((150, 150))
-        return ImageTk.PhotoImage(img)
+        self.img = Image.open("image/Guitarra.png")
+        super().imagen()
+        
 
 class Bajo(Instrumento):
     def imagen(self):
-        img = Image.open("image/Bajo.png")
-        img = img.resize((150, 150))
-        return ImageTk.PhotoImage(img)
+        self.img = Image.open("image/Bajo.png")
+        super().imagen()
 
 class Bateria(Instrumento):
     def imagen(self):
-        img = Image.open("image/Bateria.png")
-        img = img.resize((150, 150))
-        return ImageTk.PhotoImage(img)
+        self.img = Image.open("image/Bateria.png")
+        super().imagen()
 
 class Piano(Instrumento):
     def imagen(self):
-        img = Image.open("image/Piano.png")
-        img = img.resize((150, 150))
-        return ImageTk.PhotoImage(img)
+        self.img = Image.open("image/Piano.png")
+        super().imagen()
 
 class Saxofon(Instrumento):
     def imagen(self):
-        img = Image.open("image/Saxofon.png")
-        img = img.resize((150, 150))
-        return ImageTk.PhotoImage(img)
-
+        self.img = Image.open("image/Saxofon.png")
+        super().imagen()
     
